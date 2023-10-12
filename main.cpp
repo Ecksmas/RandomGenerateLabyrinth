165da61b74c593682cd705adef530de6963007cc
Ludwig <ludde.oelker@gmail.com>
2023-10-12 11:15:30 +0200

    Typo

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ int main() {
     // Initializing integers that will be used for height and width of the grid
     int width = 0, height = 0;
 
-    // Initializing and declaring the array that holds all the ASCII value for the labyrinth
+    // Initializing and declaring the array that holds all the ASCII values for the labyrinth
     int codeArr[12] = { 185,186,187,
                         188,200,201,
                         202,203,204,
                         205,206,32 };
 
-    // Enables random number on every runtime/loop
+    // Enables random numbers on every runtime/loop
     srand(time(0));
 
-    // Asks the users for the width and height of the array and the stores them in their corresponding integers
+    // Asks the users for the width and height of the array and then stores them in their corresponding integers
     std::cout << "Enter the labyrinth's width: ";
     std::cin >> width;
     std::cout << "Enter the labyrinth's height: ";
@@ -27,7 +27,7 @@ int main() {
     std::cout << "\n";
 
 
-    // Loops through the two array with users input representing the height and width
+    // Loops through the two array's with the users input representing the height and width
     for (int i = 0; i < height; ++i) {
         std::cout << "\t";
         for (int j = 0; j < width; ++j) {
@@ -35,7 +35,7 @@ int main() {
             // and then converts them to a char which is printed width amount of times
             std::cout << (char)codeArr[rand() % 12];
         }
-        // Creates a new line after width amount is printed on the line,
+        // Creates a new line after width amount is printed on a line,
         // and then returns to the top to print a new line
         std::cout << "\n";
     }
